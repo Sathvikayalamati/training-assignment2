d1ba00725cc0f27a553727373288a53c5cc679e0
Sathvikayalamati <yalamatisathvika16@gmail.com>
2025-01-08 10:27:37 +0530

    push

---
+++ looping.js
@@ -0,0 +1,22 @@
+
+// function checkEvenOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log(number + " is even.");
+//     } else {
+//         console.log(number + " is odd.");
+//     }
+// }
+
+
+// const number = 7; 
+// checkEvenOdd(number);
+let n=100;
+for(let i=0;i<=n;i++){
+    if(i%2==0){
+        console.log(i+ "is a even number");
+
+    }else{
+        console.log(i+"is a odd number");
+    }
+
+}
